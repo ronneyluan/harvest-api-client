09f4dab0aff01666ec2a41c5fb4cd461cf82b37d
Ronney Bezerra <ronneyluan@gmail.com>
2018-06-20 22:10:22 -0300

    Setup Harvest api client gem.

---
+++ lib/harvest/api/client.rb
@@ -0,0 +1,9 @@
+require "harvest/api/client/version"
+
+module Harvest
+  module Api
+    module Client
+      # Your code goes here...
+    end
+  end
+end
---
+++ spec/harvest/api/client_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Harvest::Api::Client do
+  it "has a version number" do
+    expect(Harvest::Api::Client::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
