b1b639d6fa903e8324c3b88bd5b3c7a3fa39c521
Ronney Bezerra <ronneyluan@gmail.com>
2018-06-22 19:00:32 -0300

    Adds spec configuration.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,7 @@
 require "bundler/setup"
 require "harvest/api/client"
+require "dotenv"
+require "vcr"
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
@@ -10,5 +12,19 @@ RSpec.configure do |config|
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
+  end
+
+  config.before(:suite) do
+    Dotenv.load('.env')
+
+    Harvest::Api.setup do |config|
+      config.harvest_access_token = ENV['HARVEST_ACCESS_TOKEN']
+      config.harvest_account_id = ENV['HARVEST_ACCOUNT_ID']
+    end
+
+    VCR.configure do |config|
+      config.cassette_library_dir = "spec/vcr_cassettes"
+      config.hook_into :webmock
+    end
   end
 end
